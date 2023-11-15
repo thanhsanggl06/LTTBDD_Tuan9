d7c17c5c72fcb148ab4f7806782736b7fd046b21
Thanh Sang <thanhsanggl06@gmail.com>
2023-11-15 22:01:03 +0700

    tuan 9

--- App.js
+++ App.js
@@ -1,7 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createStore } from "redux";
 
 export default function App() {
+  // ACTIONS
+  const increment = () => {
+    return {
+      type: "INCREMENT",
+    };
+  };
+  const decrement = () => {
+    return {
+      type: "DECREMENT",
+    };
+  };
+  // REDUCER
+  const counter = (state = 0, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return state + 1;
+      case "DECREMENT":
+        return state - 1;
+    }
+  };
+  // STORE
+  let store = createStore(counter);
+
+  // DISPATCH
+  store.dispatch(increment());
+  store.dispatch(decrement());
+  store.dispatch(decrement());
+  // counter state result: -1
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
